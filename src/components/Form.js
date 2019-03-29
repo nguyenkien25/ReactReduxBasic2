3efe28aa740cd2de8f874ca362ae49cf1ed69470
nguyenkien27 <kiennv@nexlesoft.com>
2019-03-29 09:03:27 +0700

    baisc project

---
+++ src/components/Form.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+
+class Form extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            en: '',
+            vn: ''
+        };
+        this.onAdd = this.onAdd.bind(this);
+    }
+
+    onAdd(){
+        const { en, vn } = this.state;
+        this.props.dispatch({
+            type: 'ADD_WORD',
+            en,
+            vn
+        });
+    }
+
+    render() {
+        return(
+            <View style={styles.container}>
+                <TextInput 
+                    style={styles.textInput}
+                    value={this.state.en}
+                    onChangeText={text => this.setState({ en:text })}
+                    placeholder='English word'
+                />
+                <TextInput 
+                    style={styles.textInput}
+                    value={this.state.vn}
+                    onChangeText={text => this.setState({ vn:text })}
+                    placeholder='Meaning '
+                />
+                <TouchableOpacity onPress={this.onAdd}>
+                    <Text>Add</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+export default connect()(Form);
+
+const styles = StyleSheet.create({
+    container: {
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textInput: {
+        height: 40,
+        width: 300,
+        backgroundColor: '#E4F6D4',
+        margin: 10,
+        paddingHorizontal: 10
+    }
+});
---
+++ src/components/Main.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import Word from './Word';
+import Filter from './Filter';
+import Header from './Header';
+import Form from './Form';
+
+class Main extends Component {
+
+    getWordList(){
+        const { myFilter, myWords } = this.props;
+        if (myFilter === 'MEMORIZED') return myWords.filter(e => e.memorized);
+        if (myFilter === 'NEED_PRACTICE') return myWords.filter(e => !e.memorized);
+        return myWords;
+    }
+
+    render() {
+        return(
+            <View style={{backgroundColor: 'yellow', flex: 1, alignSelf: 'stretch', justifyContent: 'center'}}>
+                <Header/>
+                {/* <Text> { this.props.myFilter }</Text> */}
+                <View style={{flex: 10}}>
+                    { this.props.myIsAdding? <Form/> : null}
+                    <FlatList
+                        data={ this.getWordList() }
+                        renderItem={({item})=> <Word myWord={item}/>}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+                </View>
+                <Filter/>
+            </View>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        myFilter: state.filterStatus,
+        myWords: state.arrWords,
+        myIsAdding: state.isAdding
+    };
+}
+
+export default connect(mapStateToProps)(Main);
